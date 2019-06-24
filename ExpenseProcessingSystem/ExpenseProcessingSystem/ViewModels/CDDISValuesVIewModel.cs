10f05c86cd5e9f834aa28ca63bf7edd93f07c466
akio fujiwara <kikioo0407@gmail.com>
2019-06-24 09:54:04 +0800

    -CDD Instruction Sheet output function added. -Some minor fixed to report layout. -BM : Budget list output(PDF) Added.

---
+++ ExpenseProcessingSystem/ExpenseProcessingSystem/ViewModels/CDDISValuesVIewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseProcessingSystem.ViewModels
+{
+    public class CDDISValuesVIewModel
+    {
+        public DateTime VALUE_DATE { get; set; }
+        public string REFERENCE_NO { get; set; }
+        public string COMMENT { get; set; }
+        public string SECTION { get; set; }
+        public string REMARKS { get; set; }
+        public string SCHEME_NO { get; set; }
+        public string MEMO { get; set; }
+
+        public string DEBIT_CREDIT_1 { get; set; }
+        public string CCY_1 { get; set; }
+        public double AMOUNT_1 { get; set; }
+        public string CUSTOMER_ABBR_1 { get; set; }
+        public string ACCOUNT_CODE_1 { get; set; }
+        public string ACCOUNT_NO_1 { get; set; }
+        public double EXCHANGE_RATE_1 { get; set; }
+        public string CONTRA_CCY_1 { get; set; }
+        public string FUND_1 { get; set; }
+        public string CHECK_NO_1 { get; set; }
+        public DateTime AVAILABLE_DATE_1 { get; set; }
+        public string ADVICE_1 { get; set; }
+        public string DETAILS_1 { get; set; }
+        public string ENTITY_1 { get; set; }
+        public string DIVISION_1 { get; set; }
+        public double INTER_AMOUNT_1 { get; set; }
+        public double INTER_RATE_1 { get; set; }
+
+        public string DEBIT_CREDIT_2 { get; set; }
+        public string CCY_2 { get; set; }
+        public double AMOUNT_2 { get; set; }
+        public string CUSTOMER_ABBR_2 { get; set; }
+        public string ACCOUNT_CODE_2 { get; set; }
+        public string ACCOUNT_NO_2 { get; set; }
+        public double EXCHANGE_RATE_2 { get; set; }
+        public string CONTRA_CCY_2 { get; set; }
+        public string FUND_2 { get; set; }
+        public string CHECK_NO_2 { get; set; }
+        public DateTime AVAILABLE_DATE_2 { get; set; }
+        public string ADVICE_2 { get; set; }
+        public string DETAILS_2 { get; set; }
+        public string ENTITY_2 { get; set; }
+        public string DIVISION_2 { get; set; }
+        public double INTER_AMOUNT_2 { get; set; }
+        public double INTER_RATE_2 { get; set; }
+    }
+}
