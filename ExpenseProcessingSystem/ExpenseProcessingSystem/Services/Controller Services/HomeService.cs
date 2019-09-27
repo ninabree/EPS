fd4be2c170707f2a165f98daa635e80f4976a24d
ninamartin <monina.martin@fetp.ph>
2019-09-27 13:09:22 +0800

    Entry NC: Fixed PostNC bug

--- ExpenseProcessingSystem/ExpenseProcessingSystem/Services/Controller Services/HomeService.cs
+++ ExpenseProcessingSystem/ExpenseProcessingSystem/Services/Controller Services/HomeService.cs
@@ -11612,7 +11612,7 @@ namespace ExpenseProcessingSystem.Services
                         tempGbase = new gbaseContainer
                         {
                             valDate = expenseDDV.expenseDate,
-                            remarks = particular.InterPart_Particular_Title,
+                            remarks = item.GBaseRemarks,
                             maker = expenseDDV.maker,
                             approver = _context.ExpenseEntry.FirstOrDefault(x => x.Expense_ID == expID).Expense_Approver
                         };
@@ -11657,6 +11657,14 @@ namespace ExpenseProcessingSystem.Services
                         }
                         else
                         {
+                            tempGbase = new gbaseContainer
+                            {
+                                valDate = expenseDDV.expenseDate,
+                                remarks = item.GBaseRemarks,
+                                maker = expenseDDV.maker,
+                                approver = _context.ExpenseEntry.FirstOrDefault(x => x.Expense_ID == expID).Expense_Approver
+                            };
+
                             if (particular.ExpenseEntryInterEntityAccs.Count > 0)
                             {
                                 entryType = (particular.ExpenseEntryInterEntityAccs[0].Inter_Type_ID == GlobalSystemValues.NC_DEBIT) ? "D" :
